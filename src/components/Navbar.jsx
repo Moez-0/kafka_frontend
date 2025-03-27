6254be22cb9a2edc7b8639dc2ea37b9174b3f838
Moez-0 <moezlegend@gmail.com>
2025-03-27 17:15:59 +0100

    added light mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect,useState,useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext'; // Import useAuth hook
 import { RiMenu4Fill , RiMenu5Fill } from "react-icons/ri";
 import { FaCompactDisc } from "react-icons/fa6";
 import LofiPlayer from './LofiPlayer'; // Import LofiPlayer component
+import { MdDarkMode, MdLightMode } from "react-icons/md"; // Import theme icons
+
 
 const Navbar = () => {
   const { user, logout } = useAuth(); // Get user and logout from AuthContext
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = React.useState(false);
   const menuRef = useRef(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
 
   // Close the menu when clicking outside of it
   useEffect(() => {
@@ -33,23 +37,29 @@ const Navbar = () => {
   const handleMenu = () => {
     setMenuOpen(!menuOpen); // Toggle the mobile menu
   };
-
+  useEffect(() => {
+      document.documentElement.setAttribute("data-theme", theme);
+      localStorage.setItem("theme", theme);
+    }, [theme]);
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
   return (
     <nav className="navbar h-20 w-full flex items-center justify-between px-10 relative">
       {/* Logo */}
       <div className="flex items-center">
         <Link to="/" className="text-2xl">
-          <img src="logo_light.svg" alt="Logo" className="h-12 w-12 transition-colors duration-200 hover:brightness-60" />
+          <img src={theme === "dark" ? "logo_light.svg" : "logo.svg"} alt="Logo" className="h-12 w-12 transition-colors duration-200 hover:brightness-60" />
         </Link>
       </div>
 
       {/* Hamburger icon (only visible on mobile) */}
-      <div className="md:hidden md:flex items-center space-x-4">
+      <div className="md:hidden md:flex items-center space-x-4 ">
           <LofiPlayer /> {/* 🎶 Lofi Audio Toggle */}
         </div>
       <RiMenu4Fill
         onClick={handleMenu}
-        className="h-8 w-8 text-primary cursor-pointer md:hidden"
+        className="h-8 w-8 text-primary  cursor-pointer md:hidden"
       />
 
       {/* Mobile menu (visible when menuOpen is true) */}
@@ -62,9 +72,13 @@ const Navbar = () => {
           <Link to="/post" className="hover:text-pink-300 transition" onClick={() => setMenuOpen(false)}>Add Post</Link>
           <Link to="/explore" className="hover:text-pink-300 transition" onClick={() => setMenuOpen(false)}>Explore</Link>
           <Link to="/movies" className="hover:text-pink-300 transition" onClick={() => setMenuOpen(false)}>Movies</Link>
+        
           {user && (
             <Link to="/myposts" className="hover:text-pink-300 transition" onClick={() => setMenuOpen(false)}>My Posts</Link>
           )}
+            <button onClick={toggleTheme} className=" cursor-pointer ml-4 p-2 rounded-full bg-secondary text-primary hover:brightness-75">
+        {theme === "dark" ? <MdLightMode className="h-6 w-6" /> : <MdDarkMode className="h-6 w-6" />}
+      </button>
           {user ? (
             <button
               onClick={() => { handleLogout(); setMenuOpen(false); }}
@@ -87,24 +101,26 @@ const Navbar = () => {
       {/* Desktop Menu (visible on larger screens) */}
       <div className="hidden md:flex items-center space-x-4">
         <Link to="/" className="text-lg">
-          <img src="home.svg" alt="Home" className="h-8 w-8 transition-colors duration-200 hover:brightness-60" />
+          <img src={theme === "dark" ? "home.svg" : "home_light.svg"} alt="Home" className="h-8 w-8 transition-colors duration-200 hover:brightness-60 " />
         </Link>
 
         
         <Link to="/explore" className="text-lg">
-          <img src="explore.svg" alt="Explore" className="h-8 w-8 transition-colors duration-200 hover:brightness-60" />
+          <img src={theme === "dark" ? "explore.svg" : "explore_light.svg"} alt="Explore" className="h-8 w-8 transition-colors duration-200 hover:brightness-60" />
         </Link>
         <Link to="/movies" className="text-lg">
-          <img src="movie.svg" alt="Explore" className="h-8 w-8 transition-colors duration-200 hover:brightness-60" />
+          <img src={theme === "dark" ? "movie.svg" : "movie_light.svg"} alt="Explore" className="h-8 w-8 transition-colors duration-200 hover:brightness-60 " />
         </Link>
         <Link to="/post" className="text-lg">
-          <img src="add.svg" alt="Add" className="h-8 w-8 transition-colors duration-200 hover:brightness-60" />
+          <img src={theme === "dark" ? "add.svg" : "add_light.svg"} alt="Add" className="h-8 w-8 transition-colors duration-200 hover:brightness-60 " />
         </Link>
       </div>
 
       {/* Desktop Authentication links */}
       <div className="hidden md:flex items-center space-x-4">
-      
+      <button onClick={toggleTheme} className=" cursor-pointer ml-4 p-2 rounded-full bg-secondary text-primary hover:brightness-75">
+        {theme === "dark" ? <MdLightMode className="h-6 w-6" /> : <MdDarkMode className="h-6 w-6" />}
+      </button>
         {/* <Link to="/explore" className="hover:text-pink-300 transition">Explore</Link> */}
         {user && (
           <Link to="/myposts" className="hover:text-pink-300 transition">My Posts</Link>
@@ -112,7 +128,7 @@ const Navbar = () => {
         {user ? (
           <button
             onClick={() => { handleLogout(); }}
-            className="text-lg text-background bg-primary py-1 px-3 cursor-pointer border rounded-sm transition-colors duration-200 hover:bg-pink-300"
+            className=" text-lg text-background bg-primary py-1 px-3 cursor-pointer border rounded-sm transition-colors duration-200 hover:bg-pink-300"
           >
             Log out
           </button>
